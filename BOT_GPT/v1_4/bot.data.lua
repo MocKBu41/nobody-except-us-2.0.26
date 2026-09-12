f47c939f650b633a9aea4b0ab1f2a9ba7a8d6eca
MocKBu41 <egor51.28.02@gmail.com>
2026-09-12 17:24:52 +0300

    NEU bot v1.4 group attack config

---
+++ BOT_GPT/v1_4/bot.data.lua
@@ -0,0 +1,46 @@
+MaxSquadSize = 12
+OrderRotationPeriod = 600000
+SpecialPoints = 16
+
+UnitClass = {
+    Infantry = "Infantry",
+    Vehicle = "Vehicle",
+    Tank = "Tank",
+    ATTank = "ATTank",
+    ATInfantry = "ATInfantry",
+    HeavyTank = "HeavyTank",
+    Hero = "Hero"
+}
+
+NEU_BOT = {
+    ModFolder = "nobody except us 2.0.26",
+    TickMs = 1000,
+    SpawnRetrySec = 2,
+    OpeningTankDelaySec = 5,
+    AttackWaitSec = 300,
+    AttackResultCheckSec = 120,
+    NextAttackDelaySec = 5,
+    VehicleFollowDelaySec = 5,
+    InfantryReissueSec = 5,
+    AntiAirPatrolSec = 20,
+    TankReinforcementSec = 300,
+    AntiRadDelaySec = 180,
+    EnemyResponseCheckSec = 120,
+    AssaultGroups = 2
+}
+
+function readAllUnits(sq, units, army)
+    local path = "mods\\" .. NEU_BOT.ModFolder .. "\\resource\\set\\multiplayer\\units\\"
+    local files = {
+        "units_nato.set",
+        "units_ch.set",
+        "units_rus.set",
+        "units_usa.set",
+        "units_nov.set",
+        "units_ukr.set",
+        "units_wagner.set"
+    }
+    for _, name in ipairs(files) do
+        readUnitsRaw(path .. name, units, army)
+    end
+end
