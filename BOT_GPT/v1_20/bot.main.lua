ec1709a558696888afefb05b1dd1c26121578114
MocKBu41 <egor51.28.02@gmail.com>
2026-09-13 17:18:22 +0300

    BOT_GPT v1.20: enable live telemetry from main entry

--- BOT_GPT/v1_20/bot.main.lua
+++ BOT_GPT/v1_20/bot.main.lua
@@ -1,1 +1,8 @@
-return require([[/script/multiplayer/bot.v1_20.logic]])
+local N=require([[/script/multiplayer/bot.v1_20.core]])
+local T=require([[/script/multiplayer/bot.telemetry]])
+T.install(N)
+
+require([[/script/multiplayer/bot.v1_20.logic]])
+BotApi.Events:Subscribe(BotApi.Events.Quant,T.onQuant)
+N.log('v1.20 LIVE VISUALIZER TELEMETRY ACTIVE')
+return N
